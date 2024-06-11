989b6f9c517505ac4bdac5d776facdb85622f5be
Mayuri Yadav <mayuriyadav7430@gmail.com>
2024-06-11 21:30:20 +0530

    This is initial  set up project

---
+++ Article_Management_Service/src/main/java/com/example/Article_Management_Service/ArticleManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Article_Management_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleManagementServiceApplication.class, args);
+	}
+
+}
---
+++ Article_Management_Service/src/test/java/com/example/Article_Management_Service/ArticleManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Article_Management_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
